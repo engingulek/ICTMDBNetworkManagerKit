fab31c8c9b6695e2c748cb90596123830fef70f9
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-11-11 22:20:58 +0300

    created Network Path

--- Package.swift
+++ Package.swift
@@ -14,11 +14,15 @@ let package = Package(
             targets: ["ICTMDBNetworkManagerKit"]
         ),
     ],
+    dependencies: [
+           .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
+       ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ICTMDBNetworkManagerKit"
+            name: "ICTMDBNetworkManagerKit",
+            dependencies: ["Alamofire"],
         ),
 
     ]
